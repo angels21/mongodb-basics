08fccf9474dd5241e2f6a657eafb6f1f37b8c502
angels21 <solotinted@yahoo.com>
2020-04-14 22:22:05 +0100

    crud-basics

--- updateInterns.js
+++ updateInterns.js
@@ -8,12 +8,13 @@ const updateMovies = function(db, callback) {
     const newvalues = {$set:{movie:"Money Heist", year:"2019", rating:10}};
 	//update document function 
 	collection.updateOne(myquery, newvalues, function(err, result) {
-	  assert.equal(err, null);
-	  console.log("sucessfully updated one movie document");
+      assert.equal(err, null);
+      
+
+	  console.log("Sucessfully updated one movie document");
 	  console.log(result);
 	  callback(result);
 	});
 	
-
 };
 module.exports = updateMovies;
