05979ae7dec16725e408e173364f56325d056094
angels21 <solotinted@yahoo.com>
2020-04-15 10:47:29 +0100

    crud-basics

--- findInterns.js
+++ findInterns.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const findMovies = function(db, callback) {
+const findMoviesrating = function(db, callback) {
 	// Get the documents collection
 	const collection = db.collection('myMovies');
 	//find document with query rating:7
@@ -8,10 +8,35 @@ const findMovies = function(db, callback) {
 	// Find some documents
 	collection.find(query).toArray(function(err, result) {
 	  assert.equal(err, null);
-	  console.log("Found the following movie as first occurence");
+	  console.log("Found the following documents with ratings of 7");
 	  console.log(result);
 	  callback(result);
     });	
 
 };
-module.exports = findMovies;
+module.exports = findMoviesrating;
+
+const findMoviestitles = function(db, callback)  {
+	const collection = db.collection('myMovies');
+
+	collection.find({}, {projection:{_id:0, movie:1}}).toArray(function(err, result)  {
+	assert.equal(err, null);
+	console.log("Found the following movie titles");
+	console.log(result);
+	callback(result);
+});
+};
+module.exports = findMoviestitles;
+
+const findMoviesfirst = function(db, callback)  {
+	const collection = db.collection('myMovies');
+
+	collection.findOne({}, (function(err, result)  {
+	  assert.equal(err, null);
+	  console.log("Found the following record as first occurence");
+	  console.log(result);
+	  callback(result);
+      })
+    );
+};
+module.exports = findMoviesfirst;
